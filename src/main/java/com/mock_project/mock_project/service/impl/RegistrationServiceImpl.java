9e899c5670e5c7d6dcee68636e982d8e639ac390
NguyenNgocCong <86562348+NguyenNgocCong@users.noreply.github.com>
2023-09-14 09:39:47 +0700

    update

--- src/main/java/com/mock_project/mock_project/service/impl/RegistrationServiceImpl.java
+++ src/main/java/com/mock_project/mock_project/service/impl/RegistrationServiceImpl.java
@@ -4,6 +4,9 @@ package com.mock_project.mock_project.service.impl;
 import java.util.Collections;
 import java.util.Optional;
 
+import com.mock_project.mock_project.utils.JwtUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -37,30 +40,38 @@ public class RegistrationServiceImpl implements RegistrationService{
     @Transactional
     @Override
     public void registration(RegistrationDTO registrationDTO){
-
         Optional<User> user = userRepository.findByUsername(registrationDTO.getUsername());
         if (user.isPresent()) {
             throw new UserNameExistedException("Username existed!");
         }
 
         Optional<Role> role = roleRepository.findByname(registrationDTO.getRole());
-        if (role.isEmpty()) {   
-            throw new RoleNotFoundException("Role not found");  
+        if (role.isEmpty()) {
+            throw new RoleNotFoundException("Role not found");
         }
 
-        User mappedUser = new User();
-        mappedUser.setUsername(registrationDTO.getUsername());
-        mappedUser.setEmail(registrationDTO.getEmail());
-
-        mappedUser.setFullname(registrationDTO.getFullName());
-        mappedUser.setRoles(Collections.singleton(role.get()));
 
 
+        String userName = registrationDTO.getUsername();
+        String token = JwtUtils.generateToken(userName);
 
-        mappedUser.setPassword(passwordEncoder.encode(registrationDTO.getPassword()));
+        if (token != null && !token.isEmpty()) {
+            // In giá trị token ra console
+            System.out.println("Token: " + token);
 
-        userRepository.save(mappedUser);
+            User mappedUser = new User();
+            mappedUser.setUsername(registrationDTO.getUsername());
+            mappedUser.setEmail(registrationDTO.getEmail());
+            mappedUser.setFullname(registrationDTO.getFullName());
+            mappedUser.setRoles(Collections.singleton(role.get()));
+            mappedUser.setPassword(passwordEncoder.encode(registrationDTO.getPassword()));
 
+            userRepository.save(mappedUser);
+        } else {
+            // Xử lý lỗi nếu không thể tạo ra token
+            System.out.println("Lỗi");
+        }
+        // Trả về token trong phản hồi HTTP
     }
         //gọi phương thức tạo cart sau khi tạo một user mới
 
